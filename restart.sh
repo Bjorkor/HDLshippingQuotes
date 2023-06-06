135ba171ca32aad052d69d4565d6762ffccb039b
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-06-06 14:28:13 -0500

    restart.sh

---
+++ restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find the PID of the waitress process(es)
+pids=$(ps aux | grep '[w]aitress-serve' | awk '{print $2}')
+
+# If the process is running, kill it
+if [ ! -z "$pids" ]; then
+    echo "Killing existing waitress processes..."
+    kill -9 $pids
+fi
+
+# Execute the provided command
+echo "Executing command..."
+k5start -f /home/ftp/downloads/findock.keytab -- tbarker@HDLUSA.LAN sh -c 'cd /home/ftp/HDLshippingQuotes && /home/tbarker/.local/bin/waitress-serve --port=3000 app:app'
