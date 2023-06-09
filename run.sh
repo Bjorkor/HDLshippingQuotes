5279877ad5a80a2d1655f544a5fc71ca4816119c
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-06-09 13:40:50 -0500

    sdfg

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # Execute the provided command
 echo "Executing command..."
-k5start -f /home/ftp/downloads/findock.keytab -- tbarker@HDLUSA.LAN sh -c 'cd /home/ftp/HDLshippingQuotes && /home/ftp/HDLshippingQuotes/venv/bin/waitress-serve --port=3000 app:app
+k5start -f /home/ftp/downloads/findock.keytab -- tbarker@HDLUSA.LAN sh -c 'cd /home/ftp/HDLshippingQuotes && /home/ftp/HDLshippingQuotes/venv/bin/waitress-serve --port=3000 app:app'
 
