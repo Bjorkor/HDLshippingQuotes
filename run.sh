502009cd577efd5b9a4821657666a5c1663c88a8
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-06-09 13:35:21 -0500

    yes

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Author : Zara Ali
-# Copyright (c) Tutorialspoint.com
-# Script follows here:
-
-echo 'boss makes a dollar, i make a dime, thats why i shit on company time :)'
-activate="/home/ftp/flaskapp/venv/bin/activate"
-source "$activate"
-#kinit tbarker@hdlusa.lan -k -t '/home/ftp/downloads/tbarker.keytab';
-cd /home/ftp/flaskapp
-nohup waitress-serve --port=3000 app:app
+# Execute the provided command
+echo "Executing command..."
+k5start -f /home/ftp/downloads/findock.keytab -- tbarker@HDLUSA.LAN sh -c 'cd /home/ftp/HDLshippingQuotes && /home/ftp/HDLshippingQuotes/venv/bin/waitress-serve --port=3000 app:app
 
