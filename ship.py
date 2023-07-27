fac4260229d9b73ba11251bb4465d5eaee69905d
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-07-27 08:35:25 -0500

    paoss

--- ship.py
+++ ship.py
@@ -145,10 +145,7 @@ def ship(cart, state, zip, entity):
         group = x['group']
 
         if str(sku).startswith('X') or str(sku).startswith('Y'):
-            if float(weight) < 1:
-                weight = '0' + str(abs(weight))
-            else:
-                weight = str(weight)
+            pass
         if isinstance(group, int):
             uhh = '''{
                         sku:"''' + sku + '''"
