46d96d6d58a5052f9fe80f10812573d92a9a31ae
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2023-08-08 10:16:38 -0500

    reformat results

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def result():
                     charge = {'qcode': code, 'qcharge': qcharge, 'qservice': service}
                     charges.append(charge)
             else:
-                charge = {'qcode': 'No Rates Available', 'qcharge': 'No Rates Available', 'qservice': 'No Rates Available'}
+                charge = {'qcode': 'No Rates Available', 'qcharge': 0, 'qservice': 'No Rates Available'}
                 charges.append(charge)
             book[carrierTitle] = charges
     return render_template('index.html', messages=book)
